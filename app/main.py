0e65371e1cc11d3f5d9ed3cebe7af126de0423f7
deepikatalawar <122534678+deepikatalawar@users.noreply.github.com>
2024-12-30 13:41:34 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-from app.routes.query import router as query_router
+from app.routes.query import router
 
-app = FastAPI()
+app = FastAPI(
+    title="Dynamic NoSQL Query API",
+    description="An API to query MongoDB dynamically with filtering, sorting, and aggregation.",
+    version="1.0.0"
+)
 
-# Include the query route
-app.include_router(query_router)
+app.include_router(router, prefix="/api", tags=["Dynamic Query API"])
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Dynamic NoSQL Query API!"}
